2dae66a47ef437199c5c550f5cd0e2bba542159c
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-11-10 14:36:31 -0500

    Fix cross compiling

--- sasl2-sys/build.rs
+++ sasl2-sys/build.rs
@@ -82,6 +82,8 @@ fn build_sasl(metadata: &Metadata) {
     // plugins into the main library archive, so we need to request PIC in
     // CFLAGS too.
     cflags += " -fPIC";
+    let compiler = cc::Build::new().get_compiler();
+    let CC = compiler.path();
 
     let mut configure_args = vec![
         format!("--prefix={}", install_dir.display()),
@@ -113,15 +115,18 @@ fn build_sasl(metadata: &Metadata) {
         "--with-pic".into(),
         format!("CPPFLAGS={}", cppflags),
         format!("CFLAGS={}", cflags),
+        format!("CC={}", CC.to_str().unwrap_or_default()),
     ];
     if metadata.target.contains("darwin") {
         configure_args.push("--disable-macos-framework".into());
     }
     if metadata.host != metadata.target {
+        configure_args.push(format!("--build={}", metadata.host));
         configure_args.push(format!("--host={}", metadata.target));
     }
     cmd(src_dir.join("configure"), &configure_args)
         .dir(&src_dir)
+        .env("ac_cv_gssapi_supports_spnego", "yes")
         .run()
         .expect("configure failed");
 
@@ -183,8 +188,8 @@ fn build_sasl(metadata: &Metadata) {
         println!("cargo:rustc-link-lib=static=krb5support");
         // libresolv does not exist on BSD platforms; the relevant functions are
         // part of libc instead.
-        if !is_bsd {
-            //println!("cargo:rustc-link-lib=resolv")
+        if !is_bsd && metadata.host == metadata.target {
+            println!("cargo:rustc-link-lib=resolv")
         }
     }
 }
